051e8551e36cbda0f2071b4bb0d040e7272c8ea6
Mathias Hauser <mathias.hauser@env.ethz.ch>
2016-08-15 23:07:28 +0200

    rtd handling to _bg

--- regionmask/__init__.py
+++ regionmask/__init__.py
@@ -14,9 +14,9 @@ from .version import version
 __version__ = version
 
 # try to load matplotlib and set backend to Agg on rtd
-import os
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-if on_rtd:
+import os as _os
+_on_rtd = _os.environ.get("READTHEDOCS", None) == "True"
+if _on_rtd:
     import matplotlib
     matplotlib.use('Agg')
 
