7375d8e6ff77ecefda4362a645a1f900c8764c1a
Mathias Hauser <mathias.hauser@env.ethz.ch>
2018-11-14 12:01:03 +0100

    make _obtain_ne available

--- regionmask/defined_regions/__init__.py
+++ regionmask/defined_regions/__init__.py
@@ -2,4 +2,4 @@
 from .giorgi import giorgi
 from .srex import srex
 
-from .natural_earth import natural_earth, _maybe_get_column
+from .natural_earth import natural_earth, _maybe_get_column, _obtain_ne
