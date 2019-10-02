cd73633911d9e60728882f70e883cd434b526684
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-10-02 17:36:35 +0200

    dont use("Agg") on rtd

--- regionmask/__init__.py
+++ regionmask/__init__.py
@@ -8,12 +8,6 @@ __version__ = version
 # try to load matplotlib and set backend to Agg on rtd
 import os as _os
 
-_on_rtd = _os.environ.get("READTHEDOCS", None) == "True"
-if _on_rtd:
-    import matplotlib
-
-    matplotlib.use("Agg")
-
 # import
 from .core.regions import Regions_cls, Region_cls
 
