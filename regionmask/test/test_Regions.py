cca245ea8ddf96dd5188848e65fd4a06fe19fc0a
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-10-14 10:29:26 +0200

    rename test regions

--- regionmask/test/test_Regions.py
+++ regionmask/test/test_Regions.py
@@ -20,7 +20,7 @@ outl1 = ((0, 0), (0, 1), (1, 1.0), (1, 0))
 outl2 = ((0, 1), (0, 2), (1, 2.0), (1, 1))
 outlines = [outl1, outl2]
 
-r1 = Regions(outlines, numbers, names, abbrevs, name=name)
+test_regions1 = Regions(outlines, numbers, names, abbrevs, name=name)
 
 numbers = [1, 2]
 names = {1: "Unit Square1", 2: "Unit Square2"}
@@ -29,124 +29,124 @@ poly1 = Polygon(outl1)
 poly2 = Polygon(outl2)
 poly = {1: poly1, 2: poly2}
 
-r2 = Regions(poly, numbers, names, abbrevs, name=name)
+test_regions2 = Regions(poly, numbers, names, abbrevs, name=name)
 
 # numbers as array
-r3 = Regions(poly, np.array(numbers), names, abbrevs, name=name)
+test_regions3 = Regions(poly, np.array(numbers), names, abbrevs, name=name)
 
 # =============================================================================
 
 
 def test_len():
-    assert len(r1) == 2
+    assert len(test_regions1) == 2
 
 
 def test_name():
-    assert r1.name == name
+    assert test_regions1.name == name
 
 
 def test_numbers():
-    assert np.allclose(r1.numbers, [0, 1])
-    assert np.allclose(r2.numbers, [1, 2])
+    assert np.allclose(test_regions1.numbers, [0, 1])
+    assert np.allclose(test_regions2.numbers, [1, 2])
 
 
 def test_names():
-    assert r1.names == ["Unit Square1", "Unit Square2"]
+    assert test_regions1.names == ["Unit Square1", "Unit Square2"]
 
 
 def test_abbrevs():
-    assert r1.abbrevs == ["uSq1", "uSq2"]
+    assert test_regions1.abbrevs == ["uSq1", "uSq2"]
 
 
 def test_coords():
-    assert np.allclose(r1.coords, [outl1, outl2])
+    assert np.allclose(test_regions1.coords, [outl1, outl2])
 
     out1 = np.vstack([outl1, outl1[0]])
     out2 = np.vstack([outl2, outl2[0]])
 
-    assert np.allclose(r2.coords, [out1, out2])
+    assert np.allclose(test_regions2.coords, [out1, out2])
 
 
 def test_polygon():
-    assert isinstance(r1.polygons, list)
-    assert isinstance(r2.polygons, list)
+    assert isinstance(test_regions1.polygons, list)
+    assert isinstance(test_regions2.polygons, list)
 
-    assert len(r1.polygons) == 2
-    assert len(r2.polygons) == 2
+    assert len(test_regions1.polygons) == 2
+    assert len(test_regions2.polygons) == 2
 
-    assert r1.polygons[0].equals(poly1)
-    assert r1.polygons[1].equals(poly2)
+    assert test_regions1.polygons[0].equals(poly1)
+    assert test_regions1.polygons[1].equals(poly2)
 
-    assert r2.polygons[0].equals(poly1)
-    assert r2.polygons[1].equals(poly2)
+    assert test_regions2.polygons[0].equals(poly1)
+    assert test_regions2.polygons[1].equals(poly2)
 
 
 def test_centroid():
-    assert np.allclose(r1.centroids, [[0.5, 0.5], [0.5, 1.5]])
+    assert np.allclose(test_regions1.centroids, [[0.5, 0.5], [0.5, 1.5]])
 
 
 def test_map_keys_one():
-    pytest.raises(KeyError, r1.__getitem__, "")
-    assert r1.map_keys(0) == 0
-    assert r1.map_keys("uSq1") == 0
-    assert r1.map_keys("Unit Square1") == 0
+    pytest.raises(KeyError, test_regions1.__getitem__, "")
+    assert test_regions1.map_keys(0) == 0
+    assert test_regions1.map_keys("uSq1") == 0
+    assert test_regions1.map_keys("Unit Square1") == 0
 
-    assert r2.map_keys(1) == 1
-    assert r2.map_keys("uSq1") == 1
-    assert r2.map_keys("Unit Square1") == 1
+    assert test_regions2.map_keys(1) == 1
+    assert test_regions2.map_keys("uSq1") == 1
+    assert test_regions2.map_keys("Unit Square1") == 1
 
 
 def test_map_keys_np_integer():
     key = np.array([1, 1])[0]
-    assert r3.map_keys(key) == 1
+    assert test_regions3.map_keys(key) == 1
 
 
 def test_map_keys_several():
 
-    assert r1.map_keys([0, 1]) == [0, 1]
-    assert r1.map_keys(("uSq1", "uSq2")) == [0, 1]
-    assert r1.map_keys(("Unit Square1", "Unit Square2")) == [0, 1]
+    assert test_regions1.map_keys([0, 1]) == [0, 1]
+    assert test_regions1.map_keys(("uSq1", "uSq2")) == [0, 1]
+    assert test_regions1.map_keys(("Unit Square1", "Unit Square2")) == [0, 1]
 
-    assert r2.map_keys([1, 2]) == [1, 2]
-    assert r2.map_keys(("uSq1", "uSq2")) == [1, 2]
-    assert r2.map_keys(("Unit Square1", "Unit Square2")) == [1, 2]
+    assert test_regions2.map_keys([1, 2]) == [1, 2]
+    assert test_regions2.map_keys(("uSq1", "uSq2")) == [1, 2]
+    assert test_regions2.map_keys(("Unit Square1", "Unit Square2")) == [1, 2]
 
 
 def test_map_keys_mixed():
-    assert r1.map_keys([0, "uSq2"]) == [0, 1]
+    assert test_regions1.map_keys([0, "uSq2"]) == [0, 1]
 
 
 def test_map_keys_unique():
-    assert r1.map_keys([0, 0, 0]) == [0]
-    assert r1.map_keys([0, 0, 0, 1]) == [0, 1]
+    assert test_regions1.map_keys([0, 0, 0]) == [0]
+    assert test_regions1.map_keys([0, 0, 0, 1]) == [0, 1]
 
 
 def test_subset_to_Region():
-    s1 = r1[0]
+    s1 = test_regions1[0]
     assert isinstance(s1, _OneRegion)
     assert s1.number == 0
     assert s1.abbrev == "uSq1"
 
-    s1 = r1["uSq1"]
+    s1 = test_regions1["uSq1"]
     assert isinstance(s1, _OneRegion)
     assert s1.number == 0
     assert s1.abbrev == "uSq1"
 
-    s1 = r1["Unit Square1"]
+    s1 = test_regions1["Unit Square1"]
     assert isinstance(s1, _OneRegion)
     assert s1.number == 0
     assert s1.abbrev == "uSq1"
 
 
 def test_subset_to_Region_np_integer():
-    s1 = r3[1]
+    s1 = test_regions3[1]
     assert isinstance(s1, _OneRegion)
     assert s1.number == 1
     assert s1.abbrev == "uSq1"
 
 
 def test_subset_to_Regions():
-    s1 = r1[[0]]
+    s1 = test_regions1[[0]]
     assert isinstance(s1, Regions)
     assert s1.numbers == [0]
     assert s1.abbrevs == ["uSq1"]
