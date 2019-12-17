5dbe2f61f3b00aadad5e81a75873b11af64d5fa7
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-12-17 21:06:28 +0100

    only load ar6 on demand

--- regionmask/defined_regions/__init__.py
+++ regionmask/defined_regions/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 
-from .ar6 import ar6, ar6_land, ar6_ocean, ar6_separate_pacific
+from .ar6 import ar6
 from .giorgi import giorgi
 from .natural_earth import _maybe_get_column, _obtain_ne, natural_earth
 from .srex import srex
--- regionmask/defined_regions/ar6.py
+++ regionmask/defined_regions/ar6.py
@@ -8,6 +8,35 @@ from ..core.regions import Regions
 
 DATA_PATH = pkg_resources.resource_filename("regionmask", "defined_regions")
 
+REPR = """
+Regions defined for the sixt IPCC assessment report
+
+Attributes
+----------
+all : Regions
+    All regions (land + ocean), regions split along the date line
+    are combined (see below).
+land : Regions
+    Land regions only, regions split along the date line
+    are combined (see below).
+ocean : Regions
+    Ocean regions only, regions split along the date line
+    are combined (see below).
+separate_pacific : Regions
+    Original definitions of the regions, no combination of the pacific
+    regions.
+
+Combined Regions
+----------------
+SPO and SPO*; EPO and EPO*; NPO and NPO*
+
+Note
+----
+The region numbers for all, land, and ocean are consistent. The
+region numbers for all and separate_pacific are not.
+
+"""
+
 
 def _combine_to_multipolygon(df, *names):
 
@@ -27,26 +56,6 @@ def _combine_to_multipolygon(df, *names):
 
 filename = os.path.join(
     DATA_PATH, "data", "AR6_WGI_referenceRegions", "AR6_WGI_referenceRegions.shp"
-)
-
-df = gp.read_file(filename)
-
-
-ar6_separate_pacific = Regions(
-    df.geometry, names=df.V2, abbrevs=df.V3, name="IPCC AR6 WGI Reference Regions"
-)
-
-
-df = _combine_to_multipolygon(df, "SPO", "SPO*")
-df = _combine_to_multipolygon(df, "EPO", "EPO*")
-df = _combine_to_multipolygon(df, "NPO", "NPO*")
-
-
-ar6 = Regions(
-    df.geometry,
-    names=df.V2,
-    abbrevs=df.V3,
-    name="IPCC AR6 WGI Reference Regions (combined Pacific regions)",
 )
 
 
@@ -97,9 +106,6 @@ land = [
 ]
 
 
-ar6_land = ar6[land]
-ar6_land.name = "IPCC AR6 WGI Reference Regions (combined Pacific regions, land only)"
-
 ocean = [
     "ARO",
     "SPO",
@@ -115,9 +121,90 @@ ocean = [
     "SOO",
 ]
 
-ar6_ocean = ar6[ocean]
-ar6_ocean.name = "IPCC AR6 WGI Reference Regions (combined Pacific regions, ocean only)"
 
-# df.drop(49)
+class ar6_cls(object):
+    """docstring for ar6"""
 
-# df.loc[48].geometry = geometry.MultiPolygon([df.loc[48].geometry, df.loc[49].geometry])
+    def __init__(self):
+        super(ar6_cls, self).__init__()
+
+        self.__df = None
+        self.__df_combined = None
+
+        self._all = None
+        self._land = None
+        self._ocean = None
+        self._separate_pacific = None
+
+    @property
+    def _df(self):
+
+        if self.__df is None:
+            self.__df = gp.read_file(filename)
+
+        return self.__df
+
+    @property
+    def _df_combined(self):
+
+        if self.__df_combined is None:
+            _df_combined = self._df.copy()
+
+            _df_combined = _combine_to_multipolygon(_df_combined, "SPO", "SPO*")
+            _df_combined = _combine_to_multipolygon(_df_combined, "EPO", "EPO*")
+            _df_combined = _combine_to_multipolygon(_df_combined, "NPO", "NPO*")
+
+            self.__df_combined = _df_combined
+
+        return self.__df_combined
+
+    @property
+    def all(self):
+        if self._all is None:
+            r = Regions(
+                self._df_combined.geometry,
+                names=self._df_combined.V2,
+                abbrevs=self._df_combined.V3,
+                name="IPCC AR6 WGI Reference Regions (combined Pacific regions)",
+            )
+            self._all = r
+        return self._all
+
+    @property
+    def land(self):
+        if self._land is None:
+            r = self.all[land]
+            r.name = (
+                "IPCC AR6 WGI Reference Regions (combined Pacific regions, land only)"
+            )
+            self._land = r
+        return self._land
+
+    @property
+    def ocean(self):
+        if self._ocean is None:
+            r = self.all[ocean]
+            r.name = (
+                "IPCC AR6 WGI Reference Regions (combined Pacific regions, ocean only)"
+            )
+            self._ocean = r
+        return self._ocean
+
+    @property
+    def separate_pacific(self):
+        if self._separate_pacific is None:
+            r = Regions(
+                self._df.geometry,
+                names=self._df.V2,
+                abbrevs=self._df.V3,
+                name="IPCC AR6 WGI Reference Regions",
+            )
+            self._separate_pacific = r
+
+        return self._separate_pacific
+
+    def __repr__(self):
+        return REPR
+
+
+ar6 = ar6_cls()
