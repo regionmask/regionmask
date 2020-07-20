4020dc6159612fecf36e90cf71b82cc361ab35fa
Mathias Hauser <mathias.hauser@env.ethz.ch>
2020-07-20 10:16:41 +0200

    add pooch capabilities

---
+++ regionmask/defined_regions/_ressources.py
@@ -0,0 +1,29 @@
+import pooch
+import geopandas as gp
+
+REMOTE_RESSOURCE = pooch.create(
+    # Use the default cache folder for the OS
+    path=pooch.os_cache("regionmask"),
+    # The remote data is on Github
+    base_url="https://github.com/mathause/regionmask/raw/master/data/",
+    registry={
+        "CMIP6_referenceRegions.zip": "d05cd29fb0d0f21e696f118efdb9e9d87815096844d52d989affd513e9f597d1",
+        "CMIP6_referenceRegions_pre_revisions.zip": "8507cef52057785117cabc83d6e03414b5994745bf7f297c179eb50507f7ee89",
+    },
+)
+
+
+def fetch_remote_shapefile(name):
+    """
+    uses pooch to cache files
+    """
+
+    # the file will be downloaded automatically the first time this is run.
+    return REMOTE_RESSOURCE.fetch(name)
+
+
+def read_remote_shapefile(name):
+
+    fname = fetch_remote_shapefile(name)
+
+    return gp.read_file("zip://" + fname)
