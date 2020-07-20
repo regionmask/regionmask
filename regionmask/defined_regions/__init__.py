be04845a1aa3ba8370643567c4fa2fa0d53af117
Mathias Hauser <mathias.hauser@env.ethz.ch>
2020-07-20 10:17:22 +0200

    init

--- regionmask/defined_regions/__init__.py
+++ regionmask/defined_regions/__init__.py
@@ -1,6 +1,8 @@
 # flake8: noqa
 
-from .ar6 import ar6
+from ._ar6 import ar6
+from ._ar6_pre_rev import _ar6_pre_revisions
 from .giorgi import giorgi
 from .natural_earth import _maybe_get_column, _obtain_ne, natural_earth
 from .srex import srex
+from . import _ressources
