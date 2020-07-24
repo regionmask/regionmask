25ebacd9bc6cd0c1ccef0d5a93a05973e95c8f68
Mathias Hauser <mathias.hauser@env.ethz.ch>
2020-07-24 15:26:32 +0200

    update hash

--- regionmask/defined_regions/_ressources.py
+++ regionmask/defined_regions/_ressources.py
@@ -7,7 +7,7 @@ REMOTE_RESSOURCE = pooch.create(
     # The remote data is on Github
     base_url="https://github.com/mathause/regionmask/raw/master/data/",
     registry={
-        "CMIP6_referenceRegions.zip": "d05cd29fb0d0f21e696f118efdb9e9d87815096844d52d989affd513e9f597d1",
+        "CMIP6_referenceRegions.zip": "258eabc4f2167631b80049050de554c83fc1785872ed12b563ce49a2b035cb6c",
         "CMIP6_referenceRegions_pre_revisions.zip": "8507cef52057785117cabc83d6e03414b5994745bf7f297c179eb50507f7ee89",
     },
 )
