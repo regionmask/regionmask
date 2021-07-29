f05862cbe8e2d6c9a22b8715e26277415cd0275b
Mathias Hauser <mathause@users.noreply.github.com>
2021-07-29 16:43:29 +0200

    use setuptools_scm and pyproj.toml (#241)
    * setuptools_scm and pyproj.toml
    
    * test no package_data
    
    * test package_data=False
    
    * test package_data=True
    
    * Update HOW_TO_RELEASE.md
    
    * Add whats new
    
    * Update HOW_TO_RELEASE.md

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-# get version
-with open("regionmask/version.py") as f:
-    line = f.readline().strip().replace(" ", "").replace('"', "")
-    version = line.split("=")[1]
-    __version__ = version
-
-setup(version=__version__)
+setup(use_scm_version={"fallback_version": "999"})
