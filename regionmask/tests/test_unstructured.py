7d9df7936a315c8f6a9bd530e376bbdfe37eea5e
AS <aaron.spring@mpimet.mpg.de>
2021-09-15 15:49:49 +0200

    add mpas and camse test

--- regionmask/tests/test_unstructured.py
+++ regionmask/tests/test_unstructured.py
@@ -20,23 +20,45 @@ def rad_to_deg(ds):
     return ds
 
 
+@pytest.mark.parametrize(
+    "url,lon_name,lat_name,nregions",
+    [
+        (
+            "https://github.com/psyplot/psy-simple/raw/master/tests/icon_test.nc",
+            "clon",
+            "clat",
+            58,
+        ),
+        (
+            "https://github.com/NCAR/geocat-datafiles/raw/main/netcdf_files/MPAS.nc",
+            "lonCell",
+            "latCell",
+            2,
+        ),
+        (
+            "https://github.com/NCAR/geocat-datafiles/raw/main/netcdf_files/camse_unstructured_grid.nc",
+            "lon",
+            "lat",
+            40,
+        ),
+    ],
+)
 @pytest.mark.parametrize("method", ["shapely", "pygeos"])
-def test_unstructured_icon(method):
+def test_unstructured_icon(url, lon_name, lat_name, nregions, method):
     """Test regionmask for unstructured grids."""
-    icon_url = "https://github.com/psyplot/psy-simple/raw/master/tests/icon_test.nc"
-    icon_path = icon_url.split("/")[-1]
+    path = url.split("/")[-1]
 
-    if not os.path.exists(icon_path):
-        os.system(f"wget {icon_url}")
+    if not os.path.exists(path):
+        os.system(f"wget {url}")
 
-    ds = xr.open_dataset(icon_path).isel(time=0, lev=0)
+    ds = xr.open_dataset(path)
     ds = rad_to_deg(ds)
 
-    region = regionmask.defined_regions.ar6.ocean
+    region = regionmask.defined_regions.ar6.all
 
     masked = region.mask(
-        ds, lon_name="clon", lat_name="clat", wrap_lon=False, method=method
+        ds, lon_name=lon_name, lat_name=lat_name, wrap_lon=False, method=method
     )
 
     ds_regions = ds.groupby(masked).mean()
-    assert ds_regions.region.size == 15
+    assert ds_regions.region.size == nregions
