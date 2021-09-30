eecf079c8d2388433e9249047ff1e1f4d6937e97
Mathias Hauser <mathias.hauser@env.ethz.ch>
2021-09-30 11:51:43 +0200

    fix indendation

--- regionmask/tests/utils.py
+++ regionmask/tests/utils.py
@@ -95,7 +95,7 @@ def get_naturalearth_region_or_skip(monkeypatch, region_name):
     try:
         region = attrgetter(region_name)(defined_regions)
     except URLError as e:
-            warnings.warn("naturalearth donwload timeout - test not run!")
-            pytest.skip()
+        warnings.warn("naturalearth donwload timeout - test not run!")
+        pytest.skip()
 
     return region
