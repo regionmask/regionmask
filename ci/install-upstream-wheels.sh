827887cd3e9956db5da6a0cb75fb0cc13e533345
Mathias Hauser <mathias.hauser@env.ethz.ch>
2023-11-22 13:50:08 +0100

    again

--- ci/install-upstream-wheels.sh
+++ ci/install-upstream-wheels.sh
@@ -21,15 +21,16 @@ python -m pip install --no-deps --upgrade --pre -i https://pypi.anaconda.org/sci
     pandas \
     xarray
 # install build dependencies (required due to `--no-build-isolation`)
-python -m pip install cython packaging
+python -m pip install cython
 # may be able to remove --no-build-isolation once numpy 2 is out
 python -m pip install --no-deps --upgrade --no-build-isolation \
-    --no-binary shapely rasterio \
-    git+https://github.com/shapely/shapely \
+    --no-binary shapely rasterio cartopy \
     git+https://github.com/SciTools/cartopy \
+    git+https://github.com/rasterio/rasterio \
+    git+https://github.com/shapely/shapely
+python -m pip install --no-deps --upgrade \
+    git+https://github.com/pypa/packaging \
     git+https://github.com/xarray-contrib/cf-xarray \
     git+https://github.com/geopandas/geopandas \
-    git+https://github.com/pypa/packaging \
     git+https://github.com/fatiando/pooch \
-    git+https://github.com/geopandas/pyogrio \
-    git+https://github.com/rasterio/rasterio
+    git+https://github.com/geopandas/pyogrio
