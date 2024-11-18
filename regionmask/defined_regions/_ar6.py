c676466b5c0259639eda249a73e3c7944731e37e
Mathias Hauser <mathias.hauser@env.ethz.ch>
2024-11-18 10:09:48 +0100

    fix import

--- regionmask/defined_regions/_ar6.py
+++ regionmask/defined_regions/_ar6.py
@@ -1,7 +1,7 @@
 from functools import cache
 
-from regionmask import Regions
 from regionmask.core._geopandas import from_geopandas
+from regionmask.core.regions import Regions
 from regionmask.defined_regions._ressources import read_remote_shapefile
 
 REPR = """
